f4f25d74dc5c08ed474e5e38e2c096b5aab115bc
Domnik Mack <domnikmack@gmail.com>
2018-07-13 17:11:39 -0400

    bst trav passing arr

--- trees/bst-traversal/bst-traversal.js
+++ trees/bst-traversal/bst-traversal.js
@@ -12,3 +12,32 @@ function postOrderTraversal(tree) {
   if(!tree) return [];
 	return postOrderTraversal(tree.left).concat(postOrderTraversal(tree.right)).concat([tree.value]);
 }
+
+//Here with passing an array;
+
+function inOrderTraverseArr(tree, array) {
+  if(tree) {
+		inOrderTraverseArr(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverseArr(tree.right, array);
+		return array;
+	}
+}
+
+function preOrderTraverseArr(tree, array) {
+  if(tree) {
+		array.push(tree.value);
+		preOrderTraverseArr(tree.left, array);
+		preOrderTraverseArr(tree.right, array);
+		return array;
+	}
+}
+
+function postOrderTraverseArr(tree, array) {
+  if(tree) {
+		postOrderTraverseArr(tree.left, array);
+		postOrderTraverseArr(tree.right, array);
+		array.push(tree.value);
+		return array;
+	}
+}
